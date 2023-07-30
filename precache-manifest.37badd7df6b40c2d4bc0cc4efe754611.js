c7bc76edfe2a1ebc4b3ff245877bec11a8cd8567
Renan <rs.miranda93@gmail.com>
2023-07-29 22:11:22 -0400

    Updates

---
+++ precache-manifest.37badd7df6b40c2d4bc0cc4efe754611.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e5c2cc90c06902c35df",
+    "url": "/React_Portfolio/static/css/main.40b446e9.chunk.css"
+  },
+  {
+    "revision": "3e5c2cc90c06902c35df",
+    "url": "/React_Portfolio/static/js/main.3e5c2cc9.chunk.js"
+  },
+  {
+    "revision": "b015c80ce72c690ea45e",
+    "url": "/React_Portfolio/static/js/1.b015c80c.chunk.js"
+  },
+  {
+    "revision": "0b3b1d1736f2c5113254",
+    "url": "/React_Portfolio/static/js/runtime~main.0b3b1d17.js"
+  },
+  {
+    "revision": "7f90bfbafeb10b830604f2148477d432",
+    "url": "/React_Portfolio/static/media/Renan-PhotoRoom.7f90bfba.png"
+  },
+  {
+    "revision": "7691c8d313f689ba4ae021ce6e676ad8",
+    "url": "/React_Portfolio/static/media/BloodTale_AMurderMystery_.7691c8d3.png"
+  },
+  {
+    "revision": "e4715db34b342d3218ac9bf8905d498b",
+    "url": "/React_Portfolio/static/media/tech-blog.e4715db3.png"
+  },
+  {
+    "revision": "4a29dfc3856c9e15946aa85654262311",
+    "url": "/React_Portfolio/static/media/noteTaker.4a29dfc3.png"
+  },
+  {
+    "revision": "738f32bccb7d17c900c5b6b83fea9542",
+    "url": "/React_Portfolio/static/media/README.738f32bc.gif"
+  },
+  {
+    "revision": "29f3c63cd5ed08be9d0e78750a9ebcde",
+    "url": "/React_Portfolio/static/media/employeeTracker.29f3c63c.png"
+  },
+  {
+    "revision": "1ac246041dd6ee80a88bf082b5bd36fe",
+    "url": "/React_Portfolio/static/media/weatherDashboard.1ac24604.png"
+  },
+  {
+    "revision": "a41438c9cdf6e2f6fa8b3e130f9245c9",
+    "url": "/React_Portfolio/static/media/CodeQuiz.a41438c9.gif"
+  },
+  {
+    "revision": "1a3a635ebceee80b0179a233cac99d7c",
+    "url": "/React_Portfolio/static/media/dayPlanner.1a3a635e.gif"
+  },
+  {
+    "revision": "0d55b663b9ea28fb7bdf5b414ec7df0b",
+    "url": "/React_Portfolio/static/media/PG.0d55b663.png"
+  },
+  {
+    "revision": "515017ce65690c26d37d7ae86fcd136f",
+    "url": "/React_Portfolio/static/media/jate.515017ce.png"
+  },
+  {
+    "revision": "0cd1072df778653e0a2f6d96f4af5c39",
+    "url": "/React_Portfolio/static/media/socialNetworkAPI.0cd1072d.png"
+  },
+  {
+    "revision": "7cf162b92646ef2b82073ad66d560630",
+    "url": "/React_Portfolio/static/media/portfolio.7cf162b9.png"
+  },
+  {
+    "revision": "d30514386f21011c67f43ddfd144090b",
+    "url": "/React_Portfolio/static/media/Resume.d3051438.pdf"
+  },
+  {
+    "revision": "7880b20bd4f4d1587b05ffaaf3131fd4",
+    "url": "/React_Portfolio/index.html"
+  }
+];
